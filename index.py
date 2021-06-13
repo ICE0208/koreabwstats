8f8d5b947161bffe88529e767807e55bd6d39153
Ice_tain <46257328+Icetain@users.noreply.github.com>
2021-06-13 20:32:28 +0900

    re

--- index.py
+++ index.py
@@ -664,7 +664,7 @@ async def on_message(message):
                 await message.channel.send("trying...(2)")
                 await message.channel.send("trying...(1)")
                 for numup in range(0,50):
-                    str_temp = str((numup+1)) + ". " + str(exec(f'm[{numup}][0]')) + " - " + str(exec(f'm[{numup}][1]'))
+                    str_temp = str((numup+1)) + ". " + str(exec(f'{m}[{numup}][0]')) + " - " + str(exec(f'{m}[{numup}][1]'))
                     await message.channel.send(str_temp)
             except:
                 await message.channel.send(f"ERROR")
